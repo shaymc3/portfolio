ab25747c26ee38936c95e9d9a461fe26acf1d2b5
shaymc3 <shay302727@gmail.com>
2025-04-20 22:52:47 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,11 +145,9 @@ for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 	  
 
-//document.getElementById(pages[i].dataset.page) === pages[i].dataset.page
-// if (this.innerHTML.toLowerCase() === pages[i].dataset.page){
-	
+
     for (let i = 0; i < pages.length; i++) {
-		console.log(pages[i]);
+		
       if (this.id === pages[i].dataset.page){
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
